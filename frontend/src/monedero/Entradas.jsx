cf4aea4b8edc49a9137eb12c8c6343d6dc8731f6
leyv4a <gabriel.leyva228902@potros.itson.edu.mx>
2024-06-20 08:36:40 -0700

    feat(monedero) add monedero entry and exit display table

--- frontend/src/monedero/Entradas.jsx
+++ frontend/src/monedero/Entradas.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import GenericTable from '../components/GenericTable';
 
 export default function Entradas() {
+
+  const [data, setData] = React.useState([])
+
+  const [isFullTable, setIsFullTable] = React.useState(false)
+
+  const handleFullTable = () => {
+    setIsFullTable(!isFullTable);
+  }
+
+  const columns = [
+    // { uid: 'id', nombre: 'Id', sortable: true },
+    // { uid: 'tipo', nombre: 'Tipo', sortable: true },
+    { uid: 'descripcion', nombre: 'Descripcion', sortable: true },
+    // { uid: 'tipo', nombre: 'Tipo', sortable: true },
+    { uid: 'cantidad', nombre: 'Cantidad', sortable: false },
+    { uid: 'fecha', nombre: 'Fecha', sortable: true },
+    { uid: 'acciones', nombre: 'Acciones', sortable: false },
+  ];
+
   return (
-    <div>Entradas</div>
+    <div className='flex gap-6 max-h-[100%] p-5 sm:flex-row  flex-col'>
+      <div className={isFullTable? 'w-[100%]': 'sm:w-[50%] w-[80%]'}>Formulario</div>
+      <div className={isFullTable? 'w-[100%]': 'sm:w-[50%] w-[80%]'}>
+        <GenericTable data={data} columns={columns} handleFullTable={handleFullTable}/>
+      </div>
+    </div>
   )
 }
