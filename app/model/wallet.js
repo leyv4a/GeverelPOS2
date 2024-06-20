8fca7e99cc477469f77ffac20c2a34082bb50a6b
leyv4a <gabriel.leyva228902@potros.itson.edu.mx>
2024-06-20 08:41:51 -0700

    feat(api) create wallet model (Create/Read)

---
+++ app/model/wallet.js
@@ -0,0 +1,22 @@
+const db = require('../database/database'); // Importa la conexión a la base de datos SQLite
+const logToFile = require('../utils/logger')
+
+class WalletModel{
+
+    static async getAll(tipo){
+        const sql = "SELECT * FROM wallet WHERE tipo =?"
+        const rows = await db.all(sql, [tipo]);
+        return rows;
+    }
+
+    static async create(tipo, descripcion,monto, fecha){
+      try {
+        const sql = "INSERT INTO cartera (tipo,descripcion,monto, fecha) VALUES (?,?,?,?)"
+        await db.run(sql, [tipo, descripcion, monto,fecha]);
+        logToFile(`Monedero actualizado :${tipo}`)
+        return { success: true, message: 'Transacción registrada exitosamente' };
+      } catch (error) {
+        return { success: false, message: error.message };
+      }
+    }
+}
