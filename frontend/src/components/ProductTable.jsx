72ef85fab1574bef8a33bc7fffc5c085470f5786
leyv4a <gabriel.leyva228902@potros.itson.edu.mx>
2024-06-24 15:50:45 -0700

    feat(ProductTable) add cantidad column custom props

--- frontend/src/components/ProductTable.jsx
+++ frontend/src/components/ProductTable.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue, Button, Input} from "@nextui-org/react";
+import { FaTrash } from "react-icons/fa";
 
 export default function ProductTable({data, handleEditarCantidad,handleRemove}) {
   const columns = [
@@ -27,7 +28,11 @@ export default function ProductTable({data, handleEditarCantidad,handleRemove}) 
     <Table 
     aria-label="POS TABLE">
     <TableHeader columns={columns}>
-    {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+    {(column) => ( 
+      column.key === 'cantidad' ?  
+      <TableColumn className='w-[150px]' key={column.key}>{column.label}</TableColumn>
+      :
+      <TableColumn className='' key={column.key}>{column.label}</TableColumn>)}
     </TableHeader>
     <TableBody emptyContent={"Aun no hay productos en el carrito"} items={data}>
     {(item) => (
@@ -35,12 +40,12 @@ export default function ProductTable({data, handleEditarCantidad,handleRemove}) 
             {(columnKey) => (
               <TableCell>
                 {columnKey === 'acciones' ? (
-                  <Button size="sm" color="error" onClick={() => handleRemove(item.id)}>
-                    Eliminar
+                  <Button size="sm" color="error" isIconOnly className="text-xl text-danger"  onClick={() => handleRemove(item.id)}>
+                    <FaTrash/>
                   </Button>
                 ) : columnKey === 'cantidad' ? (
                   <>
-                  <Input type="number" value={item.cantidad} onChange={(e) => handleEditarCantidad(item.id, e.target.value)} />
+                  <Input size='sm' className='max-w-[130px]' variant='underlined' type="number" value={item.cantidad} onChange={(e) => handleEditarCantidad(item.id, e.target.value)} />
                   </>
                 ): columnKey === 'precioVenta'  ? (
                   <>
